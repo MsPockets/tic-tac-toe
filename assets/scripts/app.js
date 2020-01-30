dde61cda472493f4df7d5ce6ee269195e81f651f
MsPockets <mcnutterika@gmail.com>
2020-01-30 15:56:37 -0500

    Add sign in feature

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,11 +3,11 @@
 const authEvents = require('./events')
 
 $(() => {
-  $('#sign-out-form').hide()
-  $('#change-password-form').hide()
+  $('#sign-out-form')
+  $('#change-password-form')
   $('#sign-up').on('submit', authEvents.onSignUp)
-  $('#sign-in').on('submit', authEvents.onSignIn)
-  $('#change-password').on('submit', authEvents.onChangePassword) 
+  $('#sign-in').on('click', authEvents.onSignIn)
+  $('#change-password').on('submit', authEvents.onChangePassword)
   $('#sign-out').on('submit', authEvents.onSignOut) 
-  $('#game-board').hide()
+  $('#game-board')
   })
