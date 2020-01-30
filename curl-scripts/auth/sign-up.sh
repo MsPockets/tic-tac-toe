7c9693cef809b732dd57a49bb9abfdc0af4557b9
MsPockets <mcnutterika@gmail.com>
2020-01-30 16:20:24 -0500

    Remove / from sign-up.sh

--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-curl "https://mspockets.github.io/tic-tac-toe/" \
-=======
 curl "https://mspockets.github.io/tic-tac-toe" \
->>>>>>> auth-setup
   --include \
   --request POST \
   --header "Content-Type: application/json" \
