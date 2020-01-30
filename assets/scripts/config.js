2f6316f4a71f7cb0711934d7e8ba3b4e6c996fc3
MsPockets <mcnutterika@gmail.com>
2020-01-30 14:48:39 -0500

    Fix sign in problem
    change element data in index.html
    change names in app.js and config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,6 +1,6 @@
 'use strict'
-
-const apiUrl = {
+let apiUrl
+const apiUrls = {
   development: 'https://tic-tac-toe-wdi.herokuapp.com',
   production: 'https://tic-tac-toe-wdi-production.herokuapp.com'
 }
