633e8cddddcc4a4c9f25b462d049a8bdb43fd8da
MsPockets <mcnutterika@gmail.com>
2020-01-30 16:23:47 -0500

    Save progress before destroying again

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,7 @@ const authEvents = require('./events')
 $(() => {
   $('#sign-out-form')
   $('#change-password-form')
-  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-up').on('click', authEvents.onSignUp)
   $('#sign-in').on('click', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
   $('#sign-out').on('submit', authEvents.onSignOut) 
--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -1,4 +1,4 @@
-curl "https://mspockets.github.io/tic-tac-toe" \
+curl "https://mspockets.github.io/tic-tac-toe/sign-up" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
