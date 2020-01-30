9cd5d658a709c8184575bcdd2d715eef26890e6c
MsPockets <mcnutterika@gmail.com>
2020-01-29 21:02:43 -0500

    Add foundation for necessary functions

---
+++ assets/scripts/events.js
@@ -0,0 +1,33 @@
+//sign up
+//sign in
+//change password
+//sign out
+$().on('click', )
+//start game
+$().on('click', )
+//reset
+$().on('click', )
+//place marker 
+$().on('click', )
+//check for wins 
+if () {}
+else{}
+//change player
+let player1 = true
+const changePlayer = () => player1 = !player1
+//tally win
+$().on('win', winCounter += 1)
+//tally loss 
+$().on('win', lossCounter += 1)
+//win ratio 
+let winRatio = Math.floor(winRatio/loseRatio)*100
+//sign up/in page
+$().hide()
+$().show()
+//instructions page
+$().hide()
+$().show()
+//game page
+$().hide()
+$().show()
+//
